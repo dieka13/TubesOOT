c79bafcce90f3ab5ae7fe6973e302f900a49e0e0
Fatih Khoirul Umam <fatih.umam@gmail.com>
2015-05-02 22:16:50 +0700

    BikinSiswa
    Belum di testing

--- TubesOOT/src/controller/Guru_controller.java
+++ TubesOOT/src/controller/Guru_controller.java
@@ -7,69 +7,233 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 import model.Guru_model;
+import view.Guru_view;
 import view.Login_guru_view;
 
 /**
  *
  * @author dieka
  */
-public class Guru_controller implements ActionListener{
-    private Login_guru_view gui_guru;
+public class Guru_controller implements ActionListener, ListSelectionListener, ChangeListener{
+    private Login_guru_view gui_login;
+    private Guru_view gui_guru;
     private Guru_model md_guru;
-    private String currentUser;
+    private String currentUserName, currentName, currentId, currentIdPel, currentSiswaId;
     private boolean isAdmin = false;
     
     public Guru_controller(){
         try {
-            gui_guru = new Login_guru_view();
+            gui_login = new Login_guru_view();
             md_guru = new Guru_model();
-            gui_guru.addActionListener(this);
-            gui_guru.setVisible(true);
-            gui_guru.getRootPane().setDefaultButton(gui_guru.getButtonLogin());
-            gui_guru.getTxtUsername().grabFocus();
+            gui_login.addActionListener(this);
+            gui_login.setVisible(true);
+            gui_login.getRootPane().setDefaultButton(gui_login.getButtonLogin());
+            gui_login.getTxtUsername().grabFocus();
+            
         } catch (SQLException | ClassNotFoundException | IllegalAccessException | InstantiationException ex) {
-            JOptionPane.showMessageDialog(gui_guru, ex.getMessage(), "Terjadi Kesalahan", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(gui_login, ex.getMessage(), "Terjadi Kesalahan", JOptionPane.ERROR_MESSAGE);
             System.exit(1);
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        if(ae.getSource() == gui_guru.getButtonLogin()){
+        if(ae.getSource() == gui_login.getButtonLogin()){
             ResultSet res = null;
             try {
-                res = md_guru.login(gui_guru.getTxtUsername().getText(), gui_guru.getTxtPassword().getText());
-                currentUser = res.getString("username");
+                res = md_guru.login(gui_login.getTxtUsername().getText(), gui_login.getTxtPassword().getText());
+                currentUserName = res.getString("username");
+                currentName = res.getString("nama");
+                currentId = res.getString("id_guru");
+                currentIdPel = res.getString("id_pelajaran");
                 isAdmin = res.getBoolean("admin");
-                gui_guru.dispose();
+                gui_login.dispose();
+                
+                gui_guru = new Guru_view();
+                gui_guru.addActionListener(this);
+                gui_guru.addListSelectionListener(this);
+                gui_guru.addChangeListener(this);
+                
+                gui_guru.getLblNamaGuru().setText(currentName+" ("+currentUserName+") ");
+                gui_guru.setVisible(true);
+                
+                refreshTabelKompomen();
+                refreshTabelSiswa();
+                
             } catch (Exception ex) {
-                JOptionPane.showMessageDialog(gui_guru, ex.toString(), "Terjadi Kesalahan", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(gui_login, ex.toString(), "Terjadi Kesalahan", JOptionPane.ERROR_MESSAGE);
+            }
+            
+        } else if (ae.getSource() == gui_guru.getBtnKompomenTambah()) {
+            try {
+                md_guru.insertKompomen(currentIdPel, gui_guru.getTxtTambahNama().getText(), gui_guru.getTxtTambahBobot().getText(), gui_guru.getTxtAreaTambahKeterangan().getText());
+                refreshTabelKompomen();
+                clearTambahKompomen();
+                
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(gui_login, ex.toString(), "Terjadi Kesalahan", JOptionPane.ERROR_MESSAGE);
+            }
+            
+        } else if (ae.getSource() == gui_guru.getBtnKompomenHapus()){
+            JTable tbl = gui_guru.getTblKompomen();
+            try {
+                md_guru.deleteKompomen(tbl.getValueAt(tbl.getSelectedRow(), 0).toString());
+                refreshTabelKompomen();
+                
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(gui_login, ex.toString(), "Terjadi Kesalahan", JOptionPane.ERROR_MESSAGE);
+            }
+            
+        } else if (ae.getSource() == gui_guru.getBtnKompomenSimpan()) {
+            JTable tbl = gui_guru.getTblKompomen();
+            try {
+                md_guru.updateKompomen(tbl.getValueAt(tbl.getSelectedRow(), 0).toString(), gui_guru.getTxtEditNama().getText(), gui_guru.getTxtEditBobot().getText(), gui_guru.getTxtAreaEditKeterangan().getText());
+                refreshTabelKompomen();
+            
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(gui_login, ex.toString(), "Terjadi Kesalahan", JOptionPane.ERROR_MESSAGE);
+            }
+            
+        } else if(ae.getSource() == gui_guru.getBtnNilai()){
+            gui_guru.getTabPaneNilai().setSelectedIndex(1);
+            
+        } else if(ae.getSource() == gui_guru.getBtnInput()){
+            JTable tbl = gui_guru.getTblNilai();
+            if(tbl.getValueAt(tbl.getSelectedRow(), 2) == null){
+                
+                try {
+                    md_guru.insertNilai(currentSiswaId, tbl.getValueAt(tbl.getSelectedRow(), 0).toString(), gui_guru.getTxtNilai().getText());
+                    gui_guru.getTxtNilai().setText("");
+                    refreshTabelNilai();
+                } catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(gui_login, ex, "Terjadi Kesalahan", JOptionPane.ERROR_MESSAGE);
+                }
+            } else {
+                
+                try {
+                    md_guru.updateNilai(currentSiswaId, tbl.getValueAt(tbl.getSelectedRow(), 0).toString(), gui_guru.getTxtNilai().getText());
+                    gui_guru.getTxtNilai().setText("");
+                    refreshTabelNilai();
+                } catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(gui_login, ex.toString(), "Terjadi Kesalahan", JOptionPane.ERROR_MESSAGE);
+                }
             }
         }
     }
 
+    
+    @Override
+    public void valueChanged(ListSelectionEvent lse) {
+        if(lse.getSource() == gui_guru.getTblKompomen().getSelectionModel() && !lse.getValueIsAdjusting()){
+            if(gui_guru.getTabPaneGuru().getSelectedIndex() == 0){
+                JTable tbl = gui_guru.getTblKompomen();
+                if(tbl.getSelectedRow() != -1){
+                    gui_guru.getTxtEditNama().setText(tbl.getValueAt(tbl.getSelectedRow(), 1).toString());
+                    gui_guru.getTxtEditBobot().setText(tbl.getValueAt(tbl.getSelectedRow(), 2).toString());
+                    gui_guru.getTxtAreaEditKeterangan().setText(tbl.getValueAt(tbl.getSelectedRow(), 3).toString());
+                } 
+            }
+            
+        } else if (lse.getSource() == gui_guru.getTblNilai().getSelectionModel() && !lse.getValueIsAdjusting()){
+            
+            JTable tbl = gui_guru.getTblNilai();
+            if(tbl.getSelectedRow() != -1){
+                gui_guru.getTxtNilai().setText(tbl.getValueAt(tbl.getSelectedRow(), 2) == null ? "" : tbl.getValueAt(tbl.getSelectedRow(), 2).toString());
+            }
+        }
+    }
+    
+    
+    @Override
+    public void stateChanged(ChangeEvent ce) {
+        if(ce.getSource() == gui_guru.getTabPaneNilai()){
+            if(gui_guru.getTabPaneNilai().getSelectedIndex() == 1){
+                try {
+                    
+                    JTable tbl = gui_guru.getTblSiswa();
+                    gui_guru.getLblNamaSiswa().setText("Nama : "+tbl.getValueAt(tbl.getSelectedRow(), 1).toString());
+                    currentSiswaId = tbl.getValueAt(tbl.getSelectedRow(), 0).toString();
+                    
+                    refreshTabelNilai();
+                    
+                } catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(gui_login, ex.toString(), "Terjadi Kesalahan", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        }
+    }
+    
     public Login_guru_view getView() {
-        return gui_guru;
+        return gui_login;
     }
 
     public Guru_model getModel() {
         return md_guru;
     }
 
-    public String getCurrentUser() {
-        return currentUser;
+    public String getCurrentUserName() {
+        return currentUserName;
     }
 
     public boolean isAdmin() {
         return isAdmin;
     }
     
+    public void refreshTabelKompomen() throws SQLException{
+        DefaultTableModel tm = (DefaultTableModel) gui_guru.getTblKompomen().getModel();
+        tm.setNumRows(0);
+
+        ResultSet rs_kompomen = md_guru.getAllKompomen(currentId);
+        while(rs_kompomen.next()){
+            tm.addRow(new Object[]{rs_kompomen.getString("id_kompomen"), rs_kompomen.getString("nama"), rs_kompomen.getString("bobot"), rs_kompomen.getString("keterangan")});
+        }
+        
+        if(tm.getRowCount() > 0){
+            gui_guru.getTblKompomen().setRowSelectionInterval(0,0);
+            gui_guru.getTabPaneKompomen().setSelectedIndex(0);
+        } else {
+            gui_guru.getTabPaneKompomen().setSelectedIndex(1);
+        }
+    }
     
+    public void refreshTabelSiswa() throws SQLException{
+        DefaultTableModel tm = (DefaultTableModel) gui_guru.getTblSiswa().getModel();
+        tm.setNumRows(0);
+        
+        ResultSet rs_siswa = md_guru.getAllSiswa();
+        while(rs_siswa.next()){
+            tm.addRow(new Object[]{rs_siswa.getString("id_siswa"), rs_siswa.getString("nama"), rs_siswa.getString("kelas")});
+        }
+        
+        if(tm.getRowCount() > 0){
+            gui_guru.getTblSiswa().setRowSelectionInterval(0,0);
+        } 
+    }
+    
+    public void refreshTabelNilai() throws SQLException{
+        DefaultTableModel tm = (DefaultTableModel) gui_guru.getTblNilai().getModel();
+        tm.setNumRows(0);
+        
+        JTable tab = gui_guru.getTblSiswa();
+        ResultSet rs_nilai = md_guru.getNilai(currentIdPel, tab.getValueAt(tab.getSelectedRow(), 0).toString());
+        while(rs_nilai.next()){
+            tm.addRow(new Object[]{rs_nilai.getString("kompomen.id_kompomen"), rs_nilai.getString("kompomen.nama"), rs_nilai.getString("nilai")});
+        }
+    }
+    
+    public void clearTambahKompomen(){
+        gui_guru.getTxtTambahBobot().setText("");
+        gui_guru.getTxtTambahNama().setText("");
+        gui_guru.getTxtAreaTambahKeterangan().setText("");
+    }
 }
--- TubesOOT/src/model/Guru_model.java
+++ TubesOOT/src/model/Guru_model.java
@@ -20,11 +20,68 @@ public class Guru_model {
     }
     
     public ResultSet login(String username, String password) throws SQLException, Exception{
-        ResultSet l = d.getData("SELECT * FROM guru WHERE username='"+username+"' AND password = '"+password+"' LIMIT 1");
+        ResultSet l = d.getData("SELECT * FROM guru JOIN pelajaran USING(id_guru) WHERE username='"+username+"' AND password = '"+password+"' LIMIT 1");
         if(!l.next()){
             throw new Exception("Login gagal, periksa kembali username dan password anda");
         } else {
             return l;
         }
     }
+    
+    public ResultSet getAllMataKuliah() throws SQLException{
+        ResultSet res = d.getData("SELECT * FROM guru JOIN pelajaran ON('id_guru')");
+        return res;
+    }
+    
+    public ResultSet getAllKompomen(String id_guru) throws SQLException{
+        ResultSet res = d.getData("SELECT * FROM kompomen JOIN pelajaran USING(id_pelajaran) WHERE id_guru ='"+id_guru+"'");
+        return res;
+    }
+    
+    public void insertKompomen(String idPel, String nama, String bobot, String ket) throws SQLException{
+        try{
+            d.query("INSERT INTO kompomen(nama, bobot, keterangan, id_pelajaran) VALUES('"+nama+"','"+bobot+"','"+ket+"', '"+idPel+"')");
+        } catch(SQLException e) {
+            throw e;
+        }
+    }
+    
+    public void deleteKompomen(String idKomp) throws SQLException{
+        d.query("DELETE FROM kompomen WHERE id_kompomen ='"+idKomp+"' LIMIT 1");
+    }
+    
+    public void updateKompomen(String idKomp, String nama, String bobot, String ket) throws SQLException{
+        d.query("UPDATE kompomen SET nama='"+nama+"', bobot='"+bobot+"', keterangan='"+ket+"' WHERE id_kompomen='"+idKomp+"' LIMIT 1");
+    }
+    
+    public ResultSet getAllSiswa() throws SQLException{
+        ResultSet rs = d.getData("SELECT * FROM siswa");
+        return rs;
+    }
+    
+    public ResultSet getNilai(String id_pelajaran, String id_siswa) throws SQLException{
+        ResultSet rs = d.getData("SELECT * FROM kompomen CROSS JOIN siswa "
+                + "LEFT JOIN nilai USING(id_siswa, id_kompomen)"
+                + " WHERE id_siswa='"+id_siswa+"'"
+                + " AND id_pelajaran='"+id_pelajaran+"'");
+        return rs;
+    }
+    
+    public void insertNilai(String id_siswa, String id_kompomen, String nilai) throws SQLException{
+        d.query("INSERT INTO nilai(id_siswa, id_kompomen, nilai) VALUES('"+id_siswa+"','"+id_kompomen+"','"+nilai+"')");
+    }
+    
+    public void updateNilai(String id_siswa, String id_kompomen, String nilai) throws SQLException{
+        d.query("UPDATE nilai SET nilai ='"+nilai+"' WHERE id_siswa='"+id_siswa+"' AND id_kompomen='"+id_kompomen+"'");
+    }
+    
+    public ResultSet getAllGuru() throws SQLException{
+        ResultSet rs = d.getData("SELECT * FROM guru");
+        return rs;
+    }
+    
+    public void insertGuru() throws SQLException{
+        d.query("INSERT INTO guru ");
+    }
+    
 }
