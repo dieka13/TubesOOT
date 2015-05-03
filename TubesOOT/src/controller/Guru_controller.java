15f419320f9ae5a44be2ea4cb035d070488d3557
Dieka Nugraha <dieka13@yahoo.co.id>
2015-05-03 07:26:26 +0700

    Merge remote-tracking branch 'origin/Siswa' into guru
    Conflicts:
    	TubesOOT/src/controller/Guru_controller.java
    	TubesOOT/src/model/Guru_model.java
    	TubesOOT/src/model/Siswa_model.java

--- TubesOOT/src/controller/Guru_controller.java
+++ TubesOOT/src/controller/Guru_controller.java
@@ -9,8 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.event.ChangeEvent;
